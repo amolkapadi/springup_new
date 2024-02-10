aeaba05ab9838787c03a6a21274dfad9c97afe94
Amol A. Kapadi <72144987+amolkapadi@users.noreply.github.com>
2024-02-10 12:40:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/****Navbar bg change on scroll */
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-dark');
+  } else {
+    nav.classList.remove('bg-dark');
+  }
+});
+
+
